1da2a6872973f86885bc7e8f0f1c8ee1170d3286
tommypacker <tommypacker@gmail.com>
2015-11-19 20:44:09 -0600

    Add status bar to the top

--- src/materialize.c
+++ src/materialize.c
@@ -2,7 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_status_bar;
 static GFont s_font;
+static GFont s_font_small;
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -12,7 +14,6 @@ static void update_time(){
   strftime(s_buffer, sizeof(s_buffer), "%I:%M", tick_time);
 
   text_layer_set_text(s_time_layer, s_buffer);
-
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
@@ -23,23 +24,36 @@ static void main_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
+  s_status_bar = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(52,5), bounds.size.w, 50));
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,45), bounds.size.w, 50));
 
   //Load custom minimal font
   s_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_REGULAR_DOS_48));
+  s_font_small = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_REGULAR_DOS_12));
 
   window_set_background_color(s_main_window, GColorVividCerulean);
+
+  //Set time text layer attributes
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
+  //Set status bar layer attributes
+  text_layer_set_background_color(s_status_bar, GColorClear);
+  text_layer_set_text_color(s_status_bar, GColorWhite);
+  text_layer_set_text(s_status_bar, "Battery status");
+  text_layer_set_font(s_status_bar, s_font_small);
+  text_layer_set_text_alignment(s_status_bar, GTextAlignmentCenter);
+
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_status_bar));
 }
 
 static void main_window_unload(Window *window){
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_status_bar);
   fonts_unload_custom_font(s_font);
 }
 
